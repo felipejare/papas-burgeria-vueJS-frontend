83e489bcf21018b70c09158b1be718f8ed8f163b
felipejare <felipeakf@gmail.com>
2023-10-25 16:52:03 -0300

    coloquei a onde eu quero colocar as foto

--- src/components/FotoComida.vue
+++ src/components/FotoComida.vue
@@ -4,6 +4,7 @@
         <p>{{ props.ingrediente.nome }}</p>
         <p>{{ props.ingrediente.preco }}</p>
         <p>{{ props.ingrediente.descricao }}</p>
+        <p><strong>quero a foto aq</strong></p>
         <img v-if="props.ingrediente.images && props.ingrediente.images.length > 0" :src="props.ingrediente.images[0].url" alt="" width="350" height="350">
       </div>
     </div>
