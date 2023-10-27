0c2027d29c30b22b2565f5ea328af1a0e4d0479a
felipejare <felipeakf@gmail.com>
2023-10-27 10:15:53 -0300

    adição de api

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:19003/api/'
+  baseURL: 'https://papas-burgueria-django-backend-dev-pfxc.1.ie-1.fl0.io/api/'
 })
 
 export default api
